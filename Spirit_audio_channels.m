994b64af4375c194c92530051940525f31db3c10
T Odaira <107455563+s1280025-T0@users.noreply.github.com>
2023-07-04 19:47:35 +0900

    Delete Spirit_audio_channels.m

--- Spirit_audio_channels.m
+++
@@ -1,7 +0,0 @@
-[y,Fs] = audioread('./Samples/sample.wav');
-
-% extract 1~2000Hz
-fpass1 = [1 2000];
-y1 = bandpass(y,fpass1,Fs);
-
-audiowrite('./Samples/out1.wav', y1, Fs); % play extracted audio
